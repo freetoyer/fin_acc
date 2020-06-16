515d471eaa735519cef1c517334d18017b43dd61
freetoyer <andrey_89-89@mail.ru>
2020-06-16 17:36:32 +0300

    added field shop to model Cheque

--- cheques/models.py
+++ cheques/models.py
@@ -1,22 +1,22 @@
 from django.db import models
 
 
-class Cheque(models.Model):
-    number = models.CharField(max_length=50, unique=True)
-    total_amount = models.DecimalField(max_digits=9, decimal_places=2)
-    time = models.DateTimeField()
-        
-    def __str__(self):
-        return self.number
-
-
 class Shop(models.Model):
-    cheque = models.OneToOneField(Cheque, on_delete=models.DO_NOTHING)
     inn = models.CharField(max_length=15)
     address = models.CharField(max_length=100)
     
     def __str__(self):
         return self.inn
+
+
+class Cheque(models.Model):
+    number = models.CharField(max_length=50, unique=True)
+    shop = models.ForeignKey(Shop, on_delete=models.DO_NOTHING, related_name='cheques')
+    summ = models.DecimalField(max_digits=9, decimal_places=2)
+    time = models.DateTimeField()
+        
+    def __str__(self):
+        return self.number
 
 
 class Product(models.Model):
@@ -27,13 +27,12 @@ class Product(models.Model):
     
 
 class Entry(models.Model):
-    cheque = models.ForeignKey(Cheque, on_delete=models.CASCADE, related_name='cheque_entries')
-    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING, related_name='product_entries')
+    cheque = models.ForeignKey(Cheque, on_delete=models.CASCADE, related_name='entries')
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING, related_name='entries')
     price = models.DecimalField(max_digits=9, decimal_places=2)
-    quantity = models.PositiveIntegerField()
-    amount = models.DecimalField(max_digits=9, decimal_places=2)
+    quantity = models.DecimalField(max_digits=9, decimal_places=2)
     
     def __str__(self):
-        return f'Product: {self.product} Price: {self.price} Quantity: {self.quantity} Amount: {self.amount}'
+        return f'Product: {self.product} Price: {self.price} Quantity: {self.quantity} Amount: {self.amount*self.quantity}'
 
 
