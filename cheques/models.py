f3be7e658ed927c0a9ecfb2fc43730b43a914554
freetoyer <andrey_89-89@mail.ru>
2020-06-16 14:15:37 +0300

    added fields to cheques app models

--- cheques/models.py
+++ cheques/models.py
@@ -2,16 +2,38 @@ from django.db import models
 
 
 class Cheque(models.Model):
-    pass
-
-
-class Entry(models.Model):
-    pass
+    number = models.CharField(max_length=50, unique=True)
+    total_amount = models.DecimalField(max_digits=9, decimal_places=2)
+    time = models.DateTimeField()
+        
+    def __str__(self):
+        return self.number
 
 
 class Shop(models.Model):
-    pass
+    cheque = models.OneToOneField(Cheque, on_delete=models.DO_NOTHING)
+    inn = models.CharField(max_length=15)
+    address = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.inn
 
 
 class Product(models.Model):
-    pass
+    name = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.name    
+    
+
+class Entry(models.Model):
+    cheque = models.ForeignKey(Cheque, on_delete=models.CASCADE, related_name='cheque_entries')
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING, related_name='product_entries')
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    amount = models.DecimalField(max_digits=9, decimal_places=2)
+    
+    def __str__(self):
+        return f'Product: {self.product} Price: {self.price} Quantity: {self.quantity} Amount: {self.amount}'
+
+
